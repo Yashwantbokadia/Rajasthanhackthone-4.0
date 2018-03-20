d6ffa74d0a697edecc40b9ecb0614d49f3c2454e
Yashwant Bokadia <bokadiayashwant@gmail.com>
2018-03-21 01:04:01 +0530

    Final Demo Website For Automation of Claim Processing System

---
+++ bsby/kiosk/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+              2017 - Demo.com
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
